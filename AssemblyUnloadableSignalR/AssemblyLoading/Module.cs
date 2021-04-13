692e7882207f246e25a79310ae91fda2ecf2899f
David Fowler <davidfowl@gmail.com>
2021-04-12 17:58:22 -0700

    Much simpler model

--- AssemblyUnloadableSignalR/AssemblyLoading/Module.cs
+++ AssemblyUnloadableSignalR/AssemblyLoading/Module.cs
@@ -15,15 +15,7 @@ namespace AssemblyUnloadableSignalR.AssemblyLoading
     {
         public static async Task Run<T>(string executeModule, Func<T, Task> action) where T : class
         {
-            // We're not awaiting the ExecuteAndUnload because that would keep anything rooted by the stack
-            // alive. This includes thing rooted by the child ALC we just created and the ALC itself when the 
-            // continuation is being executed.
-            // RunContinuationsAsynchronously is important to resume this logic in a different stack
-            var tcs = new TaskCompletionSource<WeakReference>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-            _ = ExecuteAndUnload<T>(executeModule, executeModule, action, tcs);
-
-            WeakReference hostAlcWeakRef = await tcs.Task;
+            WeakReference hostAlcWeakRef = await ExecuteAndUnload<T>(executeModule, executeModule, action, tcs);
 
             for (int i = 0; hostAlcWeakRef.IsAlive && (i < 10); i++)
             {
@@ -38,49 +30,45 @@ namespace AssemblyUnloadableSignalR.AssemblyLoading
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        static async Task ExecuteAndUnload<T>(string modulePath, string resolvePath, Func<T, Task> action, TaskCompletionSource<WeakReference> tcs) where T : class
+        static async Task<WeakReference> ExecuteAndUnload<T>(string modulePath, string resolvePath, Func<T, Task> action) where T : class
         {
-            try
+            var alc = new ModuleAssemblyLoadContext(resolvePath);
+
+            var typeDescriptorAssemblyPath = typeof(TypeDescriptor).Assembly.Location;
+            alc.LoadFromAssemblyPath(typeDescriptorAssemblyPath);
+            var netstandardShimAssemblyPath = Path.Combine(Path.GetDirectoryName(typeDescriptorAssemblyPath), "netstandard.dll");
+            alc.LoadFromAssemblyPath(netstandardShimAssemblyPath);
+
+            var alcWeakRef = new WeakReference(alc);
+
+            Assembly a = alc.LoadFromAssemblyPath(modulePath);
+
+            var assemblyResolverTool = new AssemblyResolverTool
             {
-                var alc = new ModuleAssemblyLoadContext(resolvePath);
+                dependencyContext = DependencyContext.Load(a),
 
-                var typeDescriptorAssemblyPath = typeof(TypeDescriptor).Assembly.Location;
-                alc.LoadFromAssemblyPath(typeDescriptorAssemblyPath);
-                var netstandardShimAssemblyPath = Path.Combine(Path.GetDirectoryName(typeDescriptorAssemblyPath), "netstandard.dll");
-                alc.LoadFromAssemblyPath(netstandardShimAssemblyPath);
-
-                var alcWeakRef = new WeakReference(alc);
-
-                Assembly a = alc.LoadFromAssemblyPath(modulePath);
-
-                var assemblyResolverTool = new AssemblyResolverTool
-                {
-                    dependencyContext = DependencyContext.Load(a),
-
-                    assemblyResolver = new CompositeCompilationAssemblyResolver
-                                        (new ICompilationAssemblyResolver[]
-                {
-                    new AppBaseCompilationAssemblyResolver(Path.GetDirectoryName(modulePath)),
-                    new ReferenceAssemblyPathResolver(),
-                    new PackageCompilationAssemblyResolver()
-                })
-                };
-
-                alc.Resolving += assemblyResolverTool.OnResolving;
-
-                var module = ResolveTool.ResolveInstance<T>(a);
-
-                await action(module);
-
-                alc.Resolving -= assemblyResolverTool.OnResolving;
-                alc.Unload();
-
-                tcs.TrySetResult(alcWeakRef);
-            }
-            catch (Exception ex)
+                assemblyResolver = new CompositeCompilationAssemblyResolver
+                                    (new ICompilationAssemblyResolver[]
             {
-                tcs.TrySetException(ex);
-            }
+                new AppBaseCompilationAssemblyResolver(Path.GetDirectoryName(modulePath)),
+                new ReferenceAssemblyPathResolver(),
+                new PackageCompilationAssemblyResolver()
+            })
+            };
+
+            alc.Resolving += assemblyResolverTool.OnResolving;
+
+            var module = ResolveTool.ResolveInstance<T>(a);
+
+            await action(module);
+
+            alc.Resolving -= assemblyResolverTool.OnResolving;
+            alc.Unload();
+            
+            // Yield right before leaving this method so that the only thing we capture on the stack is the weak reference
+            await Task.Yield();
+            
+            return alcWeakRef;
         }
     }
 }
